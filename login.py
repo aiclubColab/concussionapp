d2b079ed5516e6af831f30dea0c4b0656f57ddab
PAzZInDu <pasindu.karunanayaka@pyxeda.ai>
2025-08-18 16:57:06 +0530

    Update login.py

--- login.py
+++ login.py
@@ -10,7 +10,7 @@ if not st.user.is_logged_in:
     st.image(IMAGE_ADDRESS)
     if st.sidebar.button("Log in with Google", type="primary", icon=":material/login:"):
         st.login()
-    st.subheader('Please visit the App')
+        st.subheader('Please visit the App')
 
 else:
     #st.html(f"Hello, <span style='color: orange; font-weight: bold;'>{st.experimental_user.name}</span>!")
